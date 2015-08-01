114448e50031ec22177f337e9a0fa5719f563b62
bedrin <Dmitry.Bedrin@gmail.com>
2015-08-01 22:12:44 +0300

    Build jdbcsniffer.js using grunt

---
+++ Gruntfile.js
@@ -0,0 +1,95 @@
+module.exports = function (grunt) {
+    'use strict';
+    // Project configuration
+    grunt.initConfig({
+        // Metadata
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= pkg.license %> */\n',
+        // Task configuration
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+            dist: {
+                src: ['src/jdbcsniffer.js'],
+                dest: 'dist/jdbcsniffer.js'
+            }
+        },
+        includereplace: {
+            dist: {
+                options: {
+                  prefix: '//@@', // This works for HTML and JS replacements
+                  suffix: ''
+                },
+                // Files to perform replacements and includes with
+                src: 'src/*.js',
+                // Destination directory to copy files to
+                dest: 'dist/'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: '<%= includereplace.dist.dest %>src/jdbcsniffer.js',
+                dest: 'dist/jdbcsniffer.min.js'
+            }
+        },
+        jshint: {
+            options: {
+                node: true,
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                eqnull: true,
+                browser: true,
+                globals: { jQuery: true },
+                boss: true
+            },
+            gruntfile: {
+                src: 'gruntfile.js'
+            },
+            all: [
+                'src/*.js'
+            ]
+        },
+        qunit: {
+            files: ['test/**/*.html']
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib_test: {
+                files: '<%= jshint.lib_test.src %>',
+                tasks: ['jshint:lib_test', 'qunit']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks
+    //grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.loadNpmTasks('grunt-include-replace');
+
+    // Default task
+    grunt.registerTask('default', ['jshint', /*'qunit',*/ 'includereplace', 'uglify']);
+};
+
